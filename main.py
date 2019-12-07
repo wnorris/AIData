19870cb90fdfd557a6c8ed718c95524556e643e5
Wade Norris <wade.norris@ucla.edu>
2019-12-07 13:29:13 +0100

    Adding the ability to read and write from various types using a type specifier.

--- main.py
+++ main.py
@@ -3,31 +3,58 @@ from aidata import convert_python_dict_to_encoded_tf_example
 from aidata import convert_encoded_tf_example_to_python_dict
 from aidata import read_tf_records
 from aidata import write_tf_records
+from aidata import read_pickled_python_dicts
+from aidata import write_pickled_python_dicts
 from aidata import print_python_dicts
 import pickle
 
 def main():
   num_args = len(sys.argv) - 1
-  if num_args != 2 and num_args != 1:
-    print("Usage: main.py input output")
+  if num_args != 3 and num_args != 2 and num_args != 1:
+    print("Usage: main.py input [output] [pretty_print]")
     sys.exit()
 
-  encoded_tf_examples = read_tf_records([sys.argv[1]])
-  output_examples = []
-  python_dicts = []
-  for raw_record in encoded_tf_examples:
-    python_obj = convert_encoded_tf_example_to_python_dict(raw_record)
-    encoded_tf_example = convert_python_dict_to_encoded_tf_example(python_obj)
-    output_examples.append(encoded_tf_example)
-    python_dicts.append(python_obj)
+  full_python_dicts = []
+  input_splits = sys.argv[1].split(",")
+  for input_split in input_splits:
+    if len(input_split.split(":", 1)) != 2:
+      print("Type specifier required before input.")
+      sys.exit()
+    
+    input_type = input_split.split(":", 1)[0]
+    input_paths = input_split.split(":", 1)[1].split(",")  
+    if input_type == "tfrecord":
+      encoded_tf_examples = read_tf_records(input_paths)
+      python_dicts = list(map(convert_encoded_tf_example_to_python_dict, encoded_tf_examples))
+      full_python_dicts = full_python_dicts + python_dicts
+    elif input_type == "pickle":
+      python_dicts = read_pickled_python_dicts(input_paths)
+      full_python_dicts = full_python_dicts + python_dicts
+    else:
+      print("Unsupported input type: {}".format(input_type))
+      sys.exit()
+
+  if num_args < 2:
+    sys.exit()
+
+  if len(sys.argv[2].split(":", 1)) != 2:
+    print("Type specifier required before output.")
+    sys.exit()
+  output_type = sys.argv[2].split(":", 1)[0]
+  output_path = sys.argv[2].split(":", 1)[1]
+  if output_type == "tfrecord":
+    encoded_tf_examples = list(map(convert_python_dict_to_encoded_tf_example, full_python_dicts))
+    write_tf_records(output_path, encoded_tf_examples)
+  elif output_type == "pickle":
+    write_pickled_python_dicts(output_path, full_python_dicts)
+  else:
+    print("Unsupported output type: {}".format(output_type))
+    sys.exit()
+
+  if num_args < 3:
+    sys.exit()
 
   print_python_dicts(python_dicts)
-
-  if num_args == 2:
-    f = open("{}.pkl".format(sys.argv[2]), "wb")
-    pickle.dump(python_dicts, f)
-    f.close()
-    write_tf_records("{}.record".format(sys.argv[2]), output_examples)
 
 if __name__ == '__main__':
   main()
